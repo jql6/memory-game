a34d2e908657419d50e03758e1e94eab3cb76fbc
jql6 <jql6@sfu.ca>
2021-07-04 16:43:32 -0700

    Fix card sizing

--- src/components/cardList.js
+++ src/components/cardList.js
@@ -1,23 +1,31 @@
 // cardList.js
+import "./cardList.css";
 import React, { useState } from "react";
 import AppCard from "./appCard";
 import cardDict from "../cardDict";
 
 function CardsDisplay() {
   // Create a card list
-  const [cardList] = useState(["Ace of Spades"]);
+  const [cardList] = useState([
+    "Ace of Spades",
+    "Ace of Hearts",
+    "Two of Spades",
+    "King of Hearts",
+  ]);
 
   return (
-    <div className="card-list">
-      {cardList.map((cardName) => {
-        return (
-          <AppCard
-            key={cardName}
-            cardSymbol={cardDict[cardName]}
-            cardText={cardName}
-          />
-        );
-      })}
+    <div className="card-list-container">
+      <div className="card-list">
+        {cardList.map((cardName) => {
+          return (
+            <AppCard
+              key={cardName}
+              cardSymbol={cardDict[cardName]}
+              cardText={cardName}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
