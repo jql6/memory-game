6c4d0212afa7b7ca6082755ea2a1a4fd21ce80a5
jql6 <jql6@sfu.ca>
2021-07-05 11:28:24 -0700

    Add outcome screen components

---
+++ src/components/outcomeScreen.js
@@ -0,0 +1,21 @@
+// outcomeScreen.js
+import "./outcomeScreen.css";
+import React from "react";
+
+function OutcomeScreen(props) {
+  console.log(props.lose);
+  return (
+    <div
+      id="lose-screen"
+      style={{ display: props.lose | props.win ? "block" : "none" }}
+      onClick={props.resetFunction}
+    >
+      <h1>
+        {props.win ? "You win!" : props.lose ? "You lose!" : "Game in progress"}
+      </h1>
+      <p>Click to reset</p>
+    </div>
+  );
+}
+
+export default OutcomeScreen;
