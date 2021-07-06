73b9b89b50e74bc6e668f4721d3815521eab07e5
jql6 <jql6@sfu.ca>
2021-07-05 19:39:46 -0700

    Animate win screen with figure 8 motion

--- src/components/cardsDisplay.js
+++ src/components/cardsDisplay.js
@@ -37,7 +37,7 @@ function CardsDisplay(props) {
 
   const [loseState, setLoseState] = useState(false);
 
-  const [winState, setWinState] = useState(false);
+  const [winState, setWinState] = useState(true);
   // Cheat mode for debugging (you get to see clicked cards)
   const [cheat] = useState(false);
 
