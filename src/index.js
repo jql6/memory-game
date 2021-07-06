5d80bdca8b3d1d5d204c6321ba4a185e8cdf8d19
jql6 <jql6@sfu.ca>
2021-07-06 11:07:49 -0700

    Add google analytics

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import GA4React from "ga-4-react";
+const ga4react = new GA4React("G-GMERWLB8B0");
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+(async () => {
+  await ga4react.initialize();
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+})();
