6fd489e40bd0261fad7c0a97d129e271ab7ae28d
jql6 <jql6@sfu.ca>
2021-07-05 17:06:47 -0700

    Trying utf-8 to standardize unicode special symbols

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
       <Helmet>
         <title>Memory game</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charset="UTF-8" />
       </Helmet>
       <AppHeader currentScoreValue={currentScore} highScoreValue={highScore} />
       <CardsDisplay
